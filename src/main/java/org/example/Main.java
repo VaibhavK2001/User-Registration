f29de7e7a40ee78ee13daf7e781aac6c709cd04a
VaibhavKumbhar75 <131513816+VaibhavKumbhar75@users.noreply.github.com>
2024-04-22 16:45:23 +0530

    First Name Check

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,22 @@
 package org.example;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to User Registration Problem");
+
+        System.out.println("Enter First Name : ");
+        String fname = scanner.nextLine();
+        Boolean b = Pattern.matches("^[A-Z]{1}[a-z]{2,}$",fname);
+        if (b){
+            System.out.println(fname+" is Valid First Name");
+        }
+        else{
+            System.out.println("Invalid first name");
+        }
     }
 }
